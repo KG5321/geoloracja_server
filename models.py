2a8414784c824155bf263ad8b2bc6b3c5a0ac7d2
KG5321 <koniaxd@gmail.com>
2018-03-20 05:02:54 +0100

    Big changes, added Bootstrap

--- models.py
+++ models.py
@@ -4,3 +4,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
+    isAdmin = db.Column(db.Boolean)
+
+class Device(db.Model):
+    id = db.Column(db.String(50), primary_key = True)
+    name = db.Column(db.String(50))
+    ownerId = db.Column(db.Integer)
--- routes.py
+++ routes.py
@@ -1,8 +1,35 @@
 from server import app, db
 from models import User
-from flask import render_template
+from flask import render_template, request, url_for, redirect, session, flash, abort
+
 
 @app.route('/', methods = ['GET'])
 def index():
-    first = User.query.first()
-    return render_template('index.html')
+    if not session.get('loggedIn'):
+        return render_template('index.html')
+    else:
+        return redirect(url_for('dashboard'))
+
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+@app.route('/loginpost', methods=['POST'])
+def login_post():
+    if request.form['passwordField'] == 'asd' and request.form['emailField'] == 'a@a.pl':
+        session['loggedIn'] = True
+        return redirect(url_for('dashboard'))
+    else:
+        return 'Error'
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    if not session.get('loggedIn'):
+        return redirect(url_for('login_page'))
+    else:
+        return render_template('dashboard.html')
+
+@app.route("/logout")
+def logout():
+    session['loggedIn'] = False
+    return redirect(url_for('index'))
