83bc8b80d8d8fdc6edd3fac71f1a2ffc3382af41
Konrad Gebler <koniaxd@gmail.com>
2018-04-20 03:20:04 +0200

    Small improvements

--- server.py
+++ server.py
@@ -20,3 +20,5 @@ from routes import *
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=80)
+    #port 5000 for local development
+    #socketio.run(app, host='0.0.0.0', port=5000)
